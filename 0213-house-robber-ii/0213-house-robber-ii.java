1321fd3bc56cc6b29c9146e0617b7a1b7f2386e3
Shivraj Ghode <159439852+SGhode@users.noreply.github.com>
2026-08-14 02:44:31 +0530

    Time: 0 ms (100%), Space: 42.8 MB (61.09%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums==null || nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        
+        return Math.max(robLinear(nums, 0, nums.length-2),
+        robLinear(nums, 1, nums.length-1));    }
+        private int robLinear(int[] nums, int start, int end){
+            int prevMax = 0;
+            int currMax = 0;
+            for(int i=start;i<=end;i++){
+                int temp = currMax;
+                currMax = Math.max(currMax, prevMax + nums[i]);
+                prevMax = temp;
+            }
+            return currMax;
+        }
+}
